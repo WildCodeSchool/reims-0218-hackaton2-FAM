5be3fb2321d1444e9beee53d889bd5293d956373
Aurelie Bayre <a.bayre@gmail.com>
2018-05-17 12:33:36 +0200

    feat: Complete component

--- meet-your-star-wars-lover/src/Router.js
+++ meet-your-star-wars-lover/src/Router.js
@@ -1,49 +1,15 @@
 import React from 'react';
 import './Router.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Listepersonnes from "./Listepersonnes";
+//import Listepersonnes from "./Listepersonnes";
+import Personne from './components/Personne';
 
-const arr = [{
-  "id": 1,
-  "name": "Luke Skywalker",
-  "height": 1.72,
-  "mass": 73,
-  "gender": "male",
-  "homeworld": "tatooine",
-  "wiki": "http://starwars.wikia.com/wiki/Luke_Skywalker",
-  "image": "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg",
-  "born": -19,
-  "bornLocation": "polis massa",
-  "died": 34,
-  "diedLocation": "ahch-to",
-  "species": "human",
-  "hairColor": "blond",
-  "eyeColor": "blue",
-  "skinColor": "light",
-  "cybernetics": "Prosthetic right hand",
-  "affiliations": [
-    "Alliance to Restore the Republic",
-    "Red Squadron",
-    "Rogue Squadron",
-    "Massassi Group",
-    "Leia Organa's team",
-    "Endor strike team",
-    "Jedi Order",
-    "Bright Tree tribe",
-    "New Republic",
-    "Resistance"
-  ],
-  "masters": [
-    "Obi-Wan Kenobi",
-    "Yoda"
-  ],
-  "apprentices": [
-    "Leia Organa",
-    "Ben Solo (along with a dozen apprentices)",
-    "Rey"
-  ],
-  "formerAffiliations": []
-}]
+const luke ={
+  id: 1,
+  name: "Luke Skywalker",
+  image: "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg"
+}
+
 
 const Systemrouter = () => (
   <Router>
@@ -84,7 +50,8 @@ const Love = () => (
   <div>
     <h2>Rencontre l'amour intergalactique</h2>
     <p>avec l'api starwars</p>
-    <Listepersonnes/>
+    <Personne {...luke}>
+    </Personne>
   </div>
 );
 
--- meet-your-star-wars-lover/src/components/Personne.js
+++ meet-your-star-wars-lover/src/components/Personne.js
@@ -3,16 +3,16 @@
 import React from 'react'
 import { Row, Col, Button } from 'reactstrap'
 
-const Personne = ({id, picture, name, date}) => <div className="Row">
+const Personne = ({id, image, name}) => <div className="Row">
   <Row>
     <Col xs="12" md="4">
-      <img src={ picture } alt={ name } className="img-fluid"/>
+      <img src={ image } alt={ name } className="img-fluid"/>
     </Col>
     <Col xs="12" md="4">
       <h3> { name }</h3>
     </Col>
     <Col xs="12" md="4">
-      <Button onClick={date}>Delete</Button>
+      <Button>Date</Button>
     </Col>
   </Row>
 </div>
